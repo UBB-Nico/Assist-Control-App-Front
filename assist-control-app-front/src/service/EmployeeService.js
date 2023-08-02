8a9ea356df982947265dad98d959f94be86cb17f
Nico <nicolas.espinoza1701@alumnos.ubiobio.cl>
2023-08-02 14:50:42 -0400

    Incorporación del Frontend al repositorio Git

--- assist-control-app-front/src/service/EmployeeService.js
+++ assist-control-app-front/src/service/EmployeeService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export class EmployeeService{
 
-    url="http://localhost:8000/employee/";
+    url="http://localhost:8001/employee/";
 
     getAll(){
         return axios.get(this.url +"all").then(res => res.data);
